3e9944200609124792fcacd12fee2fa2ee35dbb9
RgAKH5 <56658138+RgAKH5@users.noreply.github.com>
2019-12-07 12:59:21 +0700

    Add files via upload
    Skrip pengirim virus via nomor telepon
    WARNING!!!
    -JANGAN DIGUNAKAN UNTUK TUJUAN YANG TAK BENAR

---
+++ kjfile.py
@@ -0,0 +1,35 @@
+#python 3.7.1
+_author_ = 'RgAKH5'
+import time
+x = time.ctime()
+print(x)
+print ("SELAMAT DATANG DI RGAKH5 SCRIPT")
+print("Email: lnagzuiqb@imail1.net")
+k = input("Masukkan nomor telepon target : ")
+o = input("masukkan pesan pesan yang ingin disampaikan kpd target........(OPSIONAL, jika tidak ada... tekan ENTER)..:	")
+import time
+print("please wait..")
+time.sleep(4)
+#penjedaan
+import time
+print("Tunggu sebentar...")
+time.sleep(3)
+print("sedang menyiapkan...")
+for u in range(4):
+  time.sleep(2)
+  print("pengiriman mulai dalam..", u)
+  time.sleep(4)
+t = 0
+while True:
+  if t < 10000000000000:
+    print("kiriman ke... : ", t ,"menuju: ",k)
+    t = t + 1
+  elif t >= 10000000000000:
+    import time
+    print("mohon tunggu...")
+    time.sleep(5)
+    print("membersihkan..")
+    time.sleep(6)
+    print("Pengiriman ke target", k ,"success")
+    break
+#OSOVKYKEOQPAOSMEIDOSJAOSKGIDKANWOIYOFOSKAKAOWKROFKGKJTKROEOWOAKEKFOYKFOXJAOSOGITJWIQOAOAODIVIVIDKSJEIAOAOCKHMEOSOSKXKYKROSOSKGITKEOAKAOAOWNTOFODKDOFOYKEOQ9OWIYKBOKCNSNEOHOSKEOYIOW9EOYI3OSORKHKVMOAKRKDIFISKAKAIWMEJDIDTHISKGKDIAIAIWIEJRJGIDIAJAIDIFIDJSUISKFKDKSOSOSOSKDKFJFJSJSKAKSKSKSCAMDKIGIEJAIAIEJRIGODJWI
